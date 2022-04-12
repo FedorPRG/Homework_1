39b7f7f4ebbf8d46a8476d8cf6629d3aa8c3308a
Fedor <fedor_prg@rambler.ru>
2022-04-12 21:10:41 +1000

    Created Lesson_4

---
+++ Lesson_4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Задача №4");
+int number1 = 5;
+int i1 = 2;
+Console.WriteLine($"Вывод четных чисел от 1 до {number1}");
+while (i1 < number1-1)
+{
+    Console.Write($"{i1}, ");
+    i1 = i1 + 2;
+}
+Console.Write($"{i1}.");
+Console.WriteLine("");
+Console.WriteLine("Задача №4");
+int number2 = 9;
+int i2 = 2;
+Console.WriteLine($"Вывод четных чисел от 1 до {number2}");
+while (i2 < number2-1)
+{
+    Console.Write($"{i2}, ");
+    i2 = i2 + 2;
+}
+Console.Write($"{i2}.");
