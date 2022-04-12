6f82700fa4330b82a867b65284bbff188f895b4c
Fedor <fedor_prg@rambler.ru>
2022-04-12 20:47:59 +1000

    Created Lesson_3

---
+++ Lesson_3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Задача №3");
+int namber1 = 4;
+if (namber1 %2 == 0)
+{
+    Console.WriteLine($"{namber1} число четное");
+}
+else Console.WriteLine($"{namber1} число не четное");
+
+Console.WriteLine("Задача №3");
+int namber2 = -3;
+if (namber2 %2 == 0)
+{
+    Console.WriteLine($"{namber2} число четное");
+}
+else Console.WriteLine($"{namber2} число не четное");
+
+Console.WriteLine("Задача №3");
+int namber3 = 7;
+if (namber3 %2 == 0)
+{
+    Console.WriteLine($"{namber3} число четное");
+}
+else Console.WriteLine($"{namber3} число не четное");
