851cc25dcf1b885e54cb1aa33c15e421ab615093
Fedor <fedor_prg@rambler.ru>
2022-04-12 20:08:29 +1000

    Created Lesson_1

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Задача №1");
+int namber1 = 5;
+int number2 = 7;
+if (namber1<number2)
+{
+    Console.WriteLine(number2);
+}
+else Console.WriteLine(namber1);
+
+Console.WriteLine("Задача №1");
+int namber3 = 2;
+int number4 = 10;
+if (namber3<number4)
+{
+    Console.WriteLine(number4);
+}
+else Console.WriteLine(namber3);
+
+Console.WriteLine("Задача №1");
+int namber5 = -9;
+int number6 = -3;
+if (namber5<number6)
+{
+    Console.WriteLine(number6);
+}
+else Console.WriteLine(namber5);
